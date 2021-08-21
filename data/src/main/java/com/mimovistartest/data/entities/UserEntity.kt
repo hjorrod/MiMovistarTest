075158a2b6522f0aa05f6a6f1a9a9fe27818abe4
hjorrod <jherrera@paradigmadigital.com>
2021-08-21 18:38:34 +0200

    search by email refactor clean logs, etc

--- data/src/main/java/com/mimovistartest/data/entities/UserEntity.kt
+++ data/src/main/java/com/mimovistartest/data/entities/UserEntity.kt
@@ -9,22 +9,6 @@ data class UserEntity(
     @PrimaryKey
     @ColumnInfo(name = "email")
     val email: String,
-//    @ColumnInfo(name = "name")
-//    val name: String,
-//    @ColumnInfo(name = "gender")
-//    val gender: String,
-//    @ColumnInfo(name = "phone")
-//    val phone: String,
-//    @ColumnInfo(name = "picture")
-//    val picture: String,
-//    @ColumnInfo(name = "street")
-//    val street: String,
-//    @ColumnInfo(name = "city")
-//    val city: String,
-//    @ColumnInfo(name = "state")
-//    val state: String,
-//    @ColumnInfo(name = "registeredDate")
-//    val registeredDate: String,
     @ColumnInfo(name = "isFav")
     val isFav: Int,
     @ColumnInfo(name = "isRemoved")
